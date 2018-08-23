3ffda3647037b8923c97b47d0c1560ca84c00b5d
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2018-08-23 19:52:26 +0300

    add sign out

--- app/src/main/java/ro/alexsicoe/clepsydra/activity/MainActivity.java
+++ app/src/main/java/ro/alexsicoe/clepsydra/activity/MainActivity.java
@@ -3,11 +3,24 @@ package ro.alexsicoe.clepsydra.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -15,6 +28,12 @@ import ro.alexsicoe.clepsydra.R;
 import ro.alexsicoe.clepsydra.util.DateTimeUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private String userEmail, userName;
+    private GoogleApiClient googleApiClient;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore rootRef;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +43,75 @@ public class MainActivity extends AppCompatActivity {
 
         GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if (googleSignInAccount != null) {
-            String userName = googleSignInAccount.getDisplayName();
+            userEmail = googleSignInAccount.getEmail();
+            userName = googleSignInAccount.getDisplayName();
             Toast.makeText(this, "Welcome " + userName, Toast.LENGTH_LONG).show();
         }
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        rootRef = FirebaseFirestore.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser == null) {//the user is not connected. Redirect to login
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_button:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signOut() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("tokenId", FieldValue.delete());
+        rootRef.collection("users").document(userEmail).update(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                firebaseAuth.signOut();
+
+                if (googleApiClient.isConnected()) {
+                    Auth.GoogleSignInApi.signOut(googleApiClient);
+                }
+            }
+        });
     }
 
     @OnClick(R.id.btnProjects)
