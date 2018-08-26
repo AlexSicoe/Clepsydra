3172273b5a3aa5b53555ae5450eaba391f0a0d85
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2018-08-26 19:27:50 +0300

    refactor drawer

--- app/src/main/java/ro/alexsicoe/clepsydra/activity/ProjectActivity.java
+++ app/src/main/java/ro/alexsicoe/clepsydra/activity/ProjectActivity.java
@@ -78,7 +78,6 @@ public class ProjectActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
