f83dd245a64b52ff582c128e60c47dcf37553084
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2018-08-26 16:55:08 +0300

    navigation drawer dummy 2

--- app/src/main/java/ro/alexsicoe/clepsydra/activity/ProjectActivity.java
+++ app/src/main/java/ro/alexsicoe/clepsydra/activity/ProjectActivity.java
@@ -2,9 +2,8 @@ package ro.alexsicoe.clepsydra.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import ro.alexsicoe.clepsydra.R;
 
@@ -21,7 +21,7 @@ public class ProjectActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project2);
+        setContentView(R.layout.activity_project);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
